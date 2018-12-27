c38adfde59a8736a4af5ffd352bdf30dcde154a6
이정권 <ditto21c@gmail.com>
2018-12-28 01:56:13 +0900

    * 파일추가

---
+++ Assets/Script/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 ClickedPos = Input.mousePosition;
+            Ray kRay = Camera.main.ScreenPointToRay(ClickedPos);
+            RaycastHit rayCastHit;
+            if (Physics.Raycast(kRay, out rayCastHit))
+            {
+                if ("Circle" == rayCastHit.collider.tag)
+                {
+                    GameMode gameMode = Camera.main.GetComponent<GameMode>();
+                    gameMode.ClickedCircle();
+                    ShowCircle(false);
+                }
+            }
+        }
+    }
+
+    public void ShowCircle(bool bShow)
+    {
+        SpriteRenderer sr = gameObject.GetComponent<SpriteRenderer>();
+        sr.enabled = bShow;
+        SphereCollider sc = gameObject.GetComponent<SphereCollider>();
+        sc.enabled = bShow;
+
+        if(bShow)
+        {
+            Vector3 pos = new Vector3(Random.Range(-3.0f, 3.0f), Random.Range(-4.0f, 4.0f), 0.0f);
+            gameObject.transform.SetPositionAndRotation(pos, new Quaternion());
+        }
+    }
+}
---
+++ Assets/Script/GameMode.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMode : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        TextObj = GameObject.FindGameObjectWithTag("Text");
+        text = TextObj.GetComponent<Text>();
+        CircleObj = GameObject.FindGameObjectWithTag("Circle");
+
+        BeginStep();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (GameStep == EGameStep.Begin)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCount();
+            }
+        }
+        else if(GameStep == EGameStep.CalcPoint)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                BeginStep();
+            }
+        }
+	}
+
+    public void ClickedCircle()
+    {
+        ++ClickedCircleCount;
+        float PlayTime = Time.fixedTime - PlayStartTime;
+        AllPlayTime += PlayTime;
+
+        if(ClickedCircleCount == 5)
+        {
+            CalcPointStep();
+        }
+        else
+        {
+            StartCount();
+        }
+    }
+
+    void StartCount()
+    {
+        GameStep = EGameStep.Count;
+        Invoke("PlayCount", 1.0f);
+        Count = 3;
+    }
+    void PlayCount()
+    {
+        TextObj.SetActive(true);
+        text.text = Count.ToString();
+        --Count;
+
+        if(-1 == Count)
+        {
+            StartCheckTime();
+        }
+        else
+        {
+            Invoke("PlayCount", 1.0f);
+        }
+    }
+    void StartCheckTime()
+    {
+        PlayStep();
+        PlayStartTime = Time.fixedTime;
+
+    }
+    void BeginStep()
+    {
+        TextObj.SetActive(true);
+        text.text = "Press Enter to Start";
+        CircleObj.SetActive(false);
+        GameStep = EGameStep.Begin;
+        ClickedCircleCount = 0;
+    }
+    void PlayStep()
+    {
+        TextObj.SetActive(false);
+        CircleObj.SetActive(true);
+        Circle circle = CircleObj.GetComponent<Circle>();
+        circle.ShowCircle(true);
+
+        GameStep = EGameStep.Play;
+    }
+    void CalcPointStep()
+    {
+        TextObj.SetActive(true);
+        CircleObj.SetActive(false);
+        GameStep = EGameStep.CalcPoint;
+
+        float PlayTimeAverage = AllPlayTime / 5.0f;
+        float PreAllTime = AllTimeAverage * AllPlayCount;
+
+        AllPlayCount += 5;
+        PreAllTime += AllPlayTime;
+
+        AllTimeAverage = PreAllTime / AllPlayCount;
+
+        string str = "PlayCount : {0}\nTime Average : {1}sec\n\nAllPlayCount : {2}\nAll Time Average : {3}sec";
+
+        object[] args = { 5, PlayTimeAverage, AllPlayCount, AllTimeAverage };
+        str = string.Format(str, args);
+        text.text = str;
+    }
+
+    int ClickedCircleCount = 0;
+    float AllPlayTime = 0.0f;
+    float PlayStartTime = 0.0f;
+
+    public enum EGameStep
+    {
+        Begin = 0,
+        Count,
+        Play,
+        CalcPoint,
+    }
+    EGameStep GameStep = EGameStep.Begin;
+    int Count;
+
+    GameObject TextObj;
+    Text text;
+    GameObject CircleObj;
+
+    int AllPlayCount = 0;
+    float AllTimeAverage = 0.0f;
+}
